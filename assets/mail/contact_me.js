6da43b40b19c80ef5d9f72e7a64e16db28267d0e
SirUsuario <alb.majora@gmail.com>
2020-12-03 22:23:45 +0100

    Correo cambiado

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -39,7 +39,7 @@ $(function () {
                         )
                         .append("</button>");
                     $("#success > .alert-success").append(
-                        "<strong>Your message has been sent. </strong>"
+                        "<strong> Mensaje enviado. </strong>"
                     );
                     $("#success > .alert-success").append("</div>");
                     //clear all fields
@@ -55,9 +55,9 @@ $(function () {
                         .append("</button>");
                     $("#success > .alert-danger").append(
                         $("<strong>").text(
-                            "Sorry " +
+                            "Lo siento " +
                                 firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
+                                ", parece que el servidor de correo no está funcionando. Inténtalo más tarde."
                         )
                     );
                     $("#success > .alert-danger").append("</div>");
